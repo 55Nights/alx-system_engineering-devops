439b1506daf8ae8d509a07398c1d6f723a7b8129
55Nights <1905587@students.kca.ke>
2022-10-20 17:00:02 +0300

    0-create

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,15 +2,7 @@
 file {'/tmp/school':
 ensure  => present,
 path    => '/tmp/school',
-<<<<<<< HEAD
-<<<<<<< HEAD
 mode    => '0744',
-=======
-mode => '0774',
->>>>>>> refs/remotes/origin/master
-=======
-mode => '0744',
->>>>>>> refs/remotes/origin/master
 owner   => 'www-data',
 group   => 'www-data',
 content => 'I love Puppet',

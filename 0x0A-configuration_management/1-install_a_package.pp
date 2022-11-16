4e9451f972fea7fb8a023200aac1008189b36e35
55Nights <1905587@students.kca.ac.ke>
2022-11-16 23:51:37 +0300

    all

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,6 @@
 # install a package
-<<<<<<< HEAD
-
-packege {'pip3':
-name => $flask,
-command => 'pip3',
-ensure => 'installed',
-
-=======
-package { 'flask':
+package {'flask':
   ensure          => 'installed',
   provider        => 'pip3',
   install_options =>  ['-v', '2.1.0']
->>>>>>> refs/remotes/origin/master
 }

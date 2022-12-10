1032d1f5397dd541553ad34a79fc11e63c4638f4
55Nights <1905587@students.kca.ac.ke>
2022-12-10 21:49:22 +0300

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module contains requests to a REST API
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    req = requests.get("https://jsonplaceholder.typicode.com/todos",
+                       {'userId': argv[1]})
+    name_req = requests.get("https://jsonplaceholder.typicode.com/users",
+                            {'id': argv[1]})
+    json_rep = req.json()
+    name = name_req.json()
+    name = name[0]["name"]
+    total_tasks = len(json_rep)
+    complete_count = 0
+    for item in json_rep:
+        if item["completed"] is True:
+            complete_count += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, complete_count, total_tasks))
+    for item in json_rep:
+        if item["completed"] is True:
+            print("\t{}".format(item["title"]))

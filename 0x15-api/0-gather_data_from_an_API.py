8e8e1338ff44826dc8548aa0399c1d4732999d9a
55Nights <1905587@students.kca.ac.ke>
2022-12-10 22:02:20 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,13 +13,14 @@ if __name__ == "__main__":
     json_rep = req.json()
     name = name_req.json()
     name = name[0]["name"]
-    total_tasks = len(json_rep)
+    total_tasks = 0
     complete_count = 0
     for item in json_rep:
+        total_tasks += 1
         if item["completed"] is True:
             complete_count += 1
     print("Employee {} is done with tasks({}/{}):"
           .format(name, complete_count, total_tasks))
     for item in json_rep:
         if item["completed"] is True:
-            print("\t{}".format(item["title"]))
+            print("\n\t{}".format(item["title"]))

c6e2cf828a7e5ea4a5c176256acc73dca6216052
55Nights <1905587@students.kca.ac.ke>
2023-01-10 11:02:11 +0300

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,21 +6,20 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    req = requests.get("https://jsonplaceholder.typicode.com/todos",
-                       {'userId': argv[1]})
-    name_req = requests.get("https://jsonplaceholder.typicode.com/users",
-                            {'id': argv[1]})
-    json_rep = req.json()
-    name = name_req.json()
-    name = name[0]["name"]
-    total_tasks = 0
-    complete_count = 0
-    for item in json_rep:
-        total_tasks += 1
-        if item["completed"] is True:
-            complete_count += 1
+    url = "https://jsonplaceholder.typicode.com/todos"
+    url_users = "https://jsonplaceholder.typicode.com/users"
+    id = argv[1]
+    tasks = requests.get(url, {"userId": id}).json()
+    user = requests.get(url_users, {"id": id}).json()
+    name = user[0]["name"]
+    complete = 0
+    total = len(tasks)
+    for task in tasks:
+        if task["completed"] is True:
+            complete = complete + 1
     print("Employee {} is done with tasks({}/{}):"
-          .format(name, complete_count, total_tasks))
-    for item in json_rep:
-        if item["completed"] is True:
-            print("\t {}".format(item["title"]))
+          .format(name, complete, total))
+    for task in tasks:
+        if task["completed"] is True:
+            title = task["title"]
+            print("\t{}".format(title))

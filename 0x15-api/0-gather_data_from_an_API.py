4f624d06347aab095295aca033384bf3ddf3b69b
55Nights <1905587@students.kca.ac.ke>
2023-02-02 17:55:02 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         if task["completed"] is True:
             total = total + 1
 
-    print("Employee {} is done with tasks({}/{})"
+    print("Employee {} is done with tasks({}/{}):"
           .format(employee_name, total, number_of_tasks)
           )
 
